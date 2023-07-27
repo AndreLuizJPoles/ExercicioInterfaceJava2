1b1f8ab7f0f9cf6fdcfd51fa91213a39c8f968fe
Andre Luiz Junqueira Poles <andre.poles@fatec.sp.gov.br>
2023-07-27 11:03:50 -0300

    Primeiro commit do exercicio

---
+++ src/application/Main.java
@@ -0,0 +1,44 @@
+package application;
+
+import model.entities.Contract;
+import model.services.ContractService;
+import model.services.PaypalService;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        try{
+            System.out.println("Entre os dados do contrato:");
+            System.out.print("Numero: ");
+            int contractNumber = sc.nextInt();
+            System.out.print("Data (dd/MM/yyyy): ");
+            LocalDate contractDate = LocalDate.parse(sc.next(), fmt);
+            System.out.print("Valor do contrato: ");
+            double contractValue = sc.nextDouble();
+            System.out.print("Entre com o numero de parcelas: ");
+            int contractNumberOfInstallments = sc.nextInt();
+            Contract contract = new Contract(contractNumber, contractDate, contractValue);
+
+            ContractService service = new ContractService(new PaypalService());
+            service.processContract(contract, contractNumberOfInstallments);
+
+            System.out.println("Parcelas:");
+            contract.printInstallments();
+        }
+        catch(InputMismatchException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+        finally {
+            sc.close();
+        }
+    }
+}
---
+++ src/model/services/ContractService.java
@@ -0,0 +1,32 @@
+package model.services;
+
+import model.entities.Contract;
+import model.entities.Installment;
+
+import java.time.LocalDate;
+
+public class ContractService {
+    private OnlinePaymentService service;
+
+    public ContractService(OnlinePaymentService service) {
+        this.service = service;
+    }
+
+    public OnlinePaymentService getService() {
+        return service;
+    }
+
+    public void setService(OnlinePaymentService service) {
+        this.service = service;
+    }
+
+    public void processContract(Contract contract, Integer months){
+        LocalDate date = contract.getDate();
+        for(int i=1;i<=months;i++){
+            double amount = contract.getTotalValue()/months + service.interest(contract.getTotalValue()/months, i);
+            double installment = amount + service.paymentFee(amount);
+            date = date.plusMonths(1);
+            contract.addInstallment(new Installment(date,installment));
+        }
+    }
+}
